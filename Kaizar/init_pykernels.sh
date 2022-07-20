0ee329c9fe18911c1a6dd42c73cf48d6f78918dd
Anthony Campbell <acampbell1798@gmail.com>
2022-07-20 21:01:21 +0000

    End of day July 20

--- Kaizar/init_pykernels.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-conda activate ds_env
-conda install -c anaconda ipykernel
-conda install -c anaconda ipywidgets
-python -m ipykernel install --user --name ds_env --display-name "Python (ds_env)"
